d3e973275982b960d103de1a68a5457d03f8c18b
Beliakov Vladimir <ermegilius@gmail.com>
2025-01-31 10:27:57 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div>
+				<Button>Hello World</Button>
+			</div>
+		</>
+	);
+}
+
+export default App;
